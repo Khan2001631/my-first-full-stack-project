936e4790561698d778d9ad607cc41094705d56c8
Mohammad Khan <fcburgers27@gmail.com>
2024-09-21 21:55:38 +0530

    Added user model

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,4 @@
-import express, { urlencoded } from 'express'
+import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
@@ -12,6 +12,7 @@ app.use(cors({
 app.use(express.json({limit: '16kb'}));  // Middleware to parse json data (limit is 16kb)
 app.use(express.urlencoded({extended: true, limit: '16kb'})); // Middleware to parse url encoded data
 app.use(express.static('public'));   // Middleware to serve static files in public folder such as favicons, pdf, images etc
+app.use(cookieParser());
 
 
 export {app}
